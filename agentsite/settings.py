02823843aeeb9820357211f646f30e0b7977aa37
nawab alam sohel <nawabalamsohel@gmail.com>
2025-10-16 21:01:43 +0600

    databse update

--- agentsite/settings.py
+++ agentsite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from pathlib import Path
+import dj_database_url
 
 
 
@@ -93,12 +94,11 @@ WSGI_APPLICATION = 'agentsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
